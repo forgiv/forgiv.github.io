f202091c996634f83ad05ffe2ddcc170668a36f6
Hiram Cruz <hiram0101@gmail.com>
2018-06-21 15:13:24 -0400

    changed bloggy live version link

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,7 +2,7 @@ const data = [
   {
     name: 'Bloggy Client',
     source: 'https://github.com/forgiv/bloggy-client',
-    live: 'https://ecstatic-mccarthy-2190d4.netlify.com/',
+    live: 'https://forgiv.github.io/bloggy/',
     description:
       'This is the client package for Bloggy. Bloggy is an open-source, centralized, blogging platform. Bloggy allows users to write posts using the powerful markdown language.'
   },
